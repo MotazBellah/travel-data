4426ea4341f11706fa6971b72a976a2c4ce9219e
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2021-01-04 21:33:03 +0200

    Create html page with form and style it

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+import crochet
+crochet.setup()
+
+from scrapy import signals
+from scrapy.crawler import CrawlerRunner
+from scrapy.signalmanager import dispatcher
+
 import os
+import sys
 from flask import Flask , render_template, jsonify, request, redirect, url_for, session, escape
+
+sys.path.insert(0, './hotels/travelData/spiders')
+
+# print(sys.path)
+from booking import BookingSpider
 
 app = Flask(__name__)
 
@@ -8,7 +21,7 @@ app.secret_key = "Super_secret_key"
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
